6eb64399a91db4b21481e86ab384bf713b86dc26
James Thorne <jmthorne101@gmail.com>
2021-06-17 21:58:20 +1200

    Add cluster issuer and ssl to api.thorney.me

--- terraform/routing.tf
+++ terraform/routing.tf
@@ -71,7 +71,7 @@ resource "aws_cloudfront_distribution" "home_distribution" {
       // These are all the defaults.
       http_port              = "80"
       https_port             = "443"
-      origin_protocol_policy = "http-only"
+      origin_protocol_policy = "https-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
 
