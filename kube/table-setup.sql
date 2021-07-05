5d4d18e88029208344fc2922694b945c773f0f16
James Thorne <jmthorne101@gmail.com>
2021-07-05 14:33:42 +1200

    Pin versions and table setup

--- kube/table-setup.sql
+++ kube/table-setup.sql
@@ -1,22 +1,25 @@
 create extension citext;
 create extension "uuid-ossp";
+create extension postgis;
 
-create table users
+create table public.users
 (
     user_id uuid default uuid_generate_v4() not null
         constraint users_pk
             primary key,
-    email   text,
-    name    text
+    email   text                            not null
 );
 
-alter table users
+alter table public.users
     owner to "james-thorne";
 
 create unique index users_user_id_uindex
-    on users (user_id);
+    on public.users (user_id);
 
-create table properties
+create unique index users_email_key
+    on public.users (email);
+
+create table public.properties
 (
     property_id   uuid default uuid_generate_v4() not null
         constraint properties_pk
@@ -26,41 +29,63 @@ create table properties
     max_price     integer,
     num_bedrooms  integer,
     num_bathrooms integer,
-    num_carparks  integer
+    num_carparks  integer,
+    land_area     integer,
+    floor_area    integer,
+    geometry      geometry
 );
 
-alter table properties
+alter table public.properties
     owner to "james-thorne";
 
-create table shared_homes
+create unique index properties_property_id_uindex
+    on public.properties (property_id);
+
+create unique index properties_address_uindex
+    on public.properties (address);
+
+create table public.shared_homes
 (
     shared_home_id uuid default uuid_generate_v4() not null
         constraint shared_homes_pk
             primary key,
-    property_id    uuid                                  not null
+    property_id    uuid                            not null
         constraint shared_homes_properties_property_id_fk
-            references properties
+            references public.properties
             on update cascade on delete cascade
 );
 
-alter table shared_homes
+alter table public.shared_homes
     owner to "james-thorne";
 
-create unique index properties_property_id_uindex
-    on properties (property_id);
-
-create table user_data
+create table public.user_data
 (
     user_id        uuid not null
         constraint user_data_users_user_id_fk
-            references users
+            references public.users
             on update cascade on delete cascade,
     shared_home_id uuid not null
         constraint user_data_shared_homes_shared_home_id_fk
-            references shared_homes
+            references public.shared_homes
             on update cascade on delete cascade
 );
 
-alter table user_data
+alter table public.user_data
+    owner to "james-thorne";
+
+create table public.locations
+(
+    location_id bigserial not null
+        constraint locations_pkey
+            primary key,
+    user_id     uuid      not null
+        constraint locations_user_id_fkey
+            references public.users
+            on update restrict on delete restrict,
+    name        text      not null,
+    geometry    geometry     not null
+);
+
+alter table public.locations
     owner to "james-thorne";
 
